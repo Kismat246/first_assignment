32df809781e853f45f0240dd2814eccdbe73dd12
Kismat246 <kismatdhoj.gurung@wmich.edu>
2025-01-17 20:22:01 -0500

    getting userinput feature added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
+#include <cctype>
+# include <algorithm>
 using namespace std;
 
-int main(){
+void getInput()
+{
+
+    bool stop = false;
+    string input;
+    string shouldContinue;
+
+    while (stop == false)
+    {
+        cout << "Enter a number of brackets: ";
+        cin >> input;
+        cout << "Your input is " << input << endl;
+        cout << "Do  you want to continue? Yes/No: ";
+        cin >> shouldContinue;
+
+        transform(shouldContinue.begin(), shouldContinue.end(), shouldContinue.begin(), ::tolower);
+
+        // cout << shouldContinue << endl;     
+
+        if (shouldContinue == "yes")
+        {
+            stop = false;
+        }
+        else if (shouldContinue == "no")
+        {
+            stop = true;
+        }else {
+            cout << "Invalid input. Assuming you want to stop" << endl;
+        }
+    }
+}
+
+
+int main()
+{
+    getInput();
     return 0;
 }
